bc72260d60c83fb6abc3d161ae91f98253a7c8ca
leondavidtb <leondavidtb@gmail.com>
2022-11-02 10:52:04 -0300

    feat: setup web and server

---
+++ web/pages/index.tsx
@@ -0,0 +1,16 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contador: {props.count}</h1>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+
+  return {
+    props: { count: data.count },
+  };
+};
